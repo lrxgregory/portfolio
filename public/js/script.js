f92f56ff799c86311018e8f730c1c2a3d6813d20
Greg <lrxgregory@gmail.com>
2019-05-11 15:47:23 +0200

    Add srolljs
    Add effects fadeInUp & fadeInDown

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,12 @@
-jQuery(function($){
-    $('div .card').on('mouseenter',function(){
-        $(this).css('border-bottom', '2px solid #57cbcc') 
-    });
+window.sr = ScrollReveal();
+        sr.reveal('.fadeInUp', {
+            duration: 1000,
+            origin:'bottom',
+            distance:'150px'
+        });
 
-    $('div .card').on('mouseleave',function(){
-        $(this).css('border-bottom', '2px solid #242930')
-    })    
-});
-
+        sr.reveal('.fadeInDown', {
+            duration: 1000,
+            origin:'top',
+            distance:'150px'
+        });donner
