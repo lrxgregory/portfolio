404a618b5861336c01806fcf7c5a76742be79cb2
lrxgregory <lrxgregory@gmail.com>
2024-09-28 14:44:51 +0200

    add translation with paraglide

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import paraglide from '@inlang/paraglide-astro';
+import { defineConfig } from 'astro/config';
 
-import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
+// Configuration complète combinant toutes les intégrations
 export default defineConfig({
-  devToolbar: {
-    enabled: false
-  },
-  integrations: [react(), tailwind(), sitemap()]
+	devToolbar: {
+		enabled: false,
+	},
+	// Configuration pour l'internationalisation avec paraglide
+	i18n: {
+		locales: ['en', 'fr', 'es'],
+		defaultLocale: 'en',
+	},
+	// Liste de toutes les intégrations
+	integrations: [
+		react(),
+		tailwind(),
+		sitemap(),
+		paraglide({
+			// recommended settings for Paraglide
+			project: './project.inlang',
+			outdir: './src/paraglide', // where your files should be generated
+		}),
+	],
 });
